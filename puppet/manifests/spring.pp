27186c52b5f3ee6396ad2853a60b9f8fe6e71d74
George H. Peden <georgep@slalom.com>
2015-08-10 16:40:46 -0700

    spring-boot-app running on host

--- puppet/manifests/spring.pp
+++ puppet/manifests/spring.pp
@@ -3,20 +3,9 @@ exec { "apt-get update":
     command => "/usr/bin/apt-get update",
 }
 
-# Web server, web app, etc.
-package { "apache2":
-  ensure => present,
-  require => Exec["apt-get update"],
+class { 'gvm' :
 }
 
-service { "apache2":
-  ensure => running,
-  require => Package["apache2"],
-}
+class { 'groovy' : 
 
-file { "/var/www/html/sample-webapp":
-  ensure => "link",
-  target => "/vagrant/sample-webapp",
-  require => Package["apache2"],
-  notify => Service["apache2"],
 }
